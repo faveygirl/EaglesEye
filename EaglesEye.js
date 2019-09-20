7dfac972b50baa250a5c0f7e324777b3b295db52
Elechi Golden <36916047+techmose@users.noreply.github.com>
2019-09-20 20:26:25 +0100

    password strenght

--- EaglesEye.js
+++ EaglesEye.js
@@ -25,3 +25,33 @@ window.onclick = function(e) {
   
 
 }
+//password strenght checker
+var strength = {
+        0: "Worst",
+        1: "Bad",
+        2: "Weak",
+        3: "Good",
+        4: "Strong"
+}
+
+var password = document.getElementById('password');
+var meter = document.getElementById('strength');
+var text = document.getElementById('strength-text');
+
+password.addEventListener('input', function()
+{
+    var val = password.value;
+    var result = zxcvbn(val);
+    
+    // Update the password strength meter
+    meter.value = result.score;
+   
+    // Update the text indicator
+    if(val !== "") {
+        text.innerHTML = "Strength: " + "<strong>" + strength[result.score] + "</strong>" + "<span class='feedback'>" + result.feedback.warning + " </span"; 
+    }
+    else {
+        text.innerHTML = "";
+    }
+});
+
